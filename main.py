da1a9c46a373f2f5900e25f517b543f86af0fc21
Netchirp <jinxynet@duck.com>
2025-11-13 16:12:02 +0100

    feat: setup initial case structure in main.py
    Adds the basic structural framework for handling user options in main.py

--- main.py
+++ main.py
@@ -37,7 +37,13 @@ while execute:
                     "[+] Introduce el estado de la tarea (Completada o Pendiente): "
                 )
                 add_task(task_to_add, status_of_task)
-
+            case 2:
+                pass
+            case 3:
+                execute = False
+                break
+            case _:  # Caso que se ejecutara si ninguno de los anteriores es valido
+                print(f"{COLOR_ROJO}Caso no esperado!{COLOR_RESET}")
     except ValueError:  # Si no es un entero
         print(
             f"{COLOR_ROJO}El valor introducido no es un numero. Introduce un numero del menu de opciones{COLOR_RESET}\n"
