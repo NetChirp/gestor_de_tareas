7f80573e860f7f6f52b326f36ed9524a13d4e265
Netchirp <jinxynet@duck.com>
2025-11-14 14:46:08 +0100

    feat(main.py): Make add_task function work
    Now, the add_task() function is able to insert tasks into the proper list, and those tasks are iterable and can be displayed in a human-readable form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Programa monolitico para la gestión de tareas
 """
-
 TODO: Refactor add_task function to use the strip and split methods to add the task.
 TODO: Refactor search_task function to search through a parameter, sorting the zip() variable results
 """
@@ -17,7 +16,7 @@ tasks = []
 * [['Tarea', 'estado'], ['Tarea', 'estado']]
 """
 
-RED_COLOR = "\033[1;31;40m"
+RED_COLOR = "\033[1;31m"
 RESET_COLOR = "\033[0m"
 BLUE_COLOR = "\033[1;34m"
 YELLOW_COLOR = "\033[33m"
@@ -25,12 +24,10 @@ YELLOW_COLOR = "\033[33m"
 
 def search_task(name: str) -> bool:
     """
-    Busca si una tarea con el 'name' dado ya existe en la lista 'tasks'.
+    Busca si una tarea con el nombre introducido ya existe en la lista tasks'.
     Asume que tasks es una lista de elementos (tuplas/listas), donde el nombre es el índice 0.
     """
-    # TODO: Remove the unnecesary status iteration
-    for task, status in tasks:
-        # Solo necistamos el nombre de la tarea
+    for task, _ in tasks:
         if task.lower() == name.lower():
             print(
                 f"{YELLOW_COLOR}La tarea ya existe! Por favor, introduce otro nombre.{RESET_COLOR}\n"
@@ -42,10 +39,16 @@ def search_task(name: str) -> bool:
 
 def add_task():
     # TODO: Test this refactorization of the code. Still needs polishing
-    task: str = input("Introduce la tarea: ")
-    status: str = input("Introduce el estado de la tarea")
-    if search_task(task):
+    user_task: str = input(
+        f"{YELLOW_COLOR}Introduce la tarea y su estado, separado por una coma{RESET_COLOR}\nAñadir: "
+    )
+    user_task_formatted = [x.strip() for x in user_task.split(",")]
+
+    if search_task(user_task):
         print("La tarea ya existe, no pueden haber duplicados.")
+    else:
+        tasks.append(user_task_formatted)
+        os.system("clear")
 
 
 def remove_task(name: str):
@@ -69,21 +72,22 @@ while execute:
         match selection:
             case 1:
                 try:
-                    os.system(
-                        "clear"
-                    )  # Targeted towards Linux / Powershell / MacOs. Won't work on cmd. Don't care
+                    os.system("clear")  #! No funcionara en el antiguo CMD de Windows
                     add_task()
-                except ValueError:
+                except ValueError:  # ? Does this except actually catch anything?
                     print("Porfavor, escribe una tarea y su estado, no numeros")
             case 2:
                 os.system("clear")
                 pass
             case 3:
                 execute = False
+                os.system("clear")
                 break
             case _:  # Caso que se ejecutara si ninguno de los anteriores es valido
                 print(f"{RED_COLOR}Caso no esperado!{RESET_COLOR}")
     except ValueError:  # Si no es un entero
+        os.system("clear")
         print(
             f"{RED_COLOR}El valor introducido no es valido. Introduce un numero del menu de opciones{RESET_COLOR}\n"
         )
+print(tasks)
